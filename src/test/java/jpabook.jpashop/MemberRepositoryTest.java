7e55245cfdf0fc1e618e5b706065d50a5bb960dd
ballack <ballack02@naver.com>
2020-12-15 23:04:55 +0900

    SpringBoot, JPA 강의 / 회원 도메인 개발

--- src/test/java/jpabook.jpashop/MemberRepositoryTest.java
+++ src/test/java/jpabook.jpashop/MemberRepositoryTest.java
@@ -1,5 +1,7 @@
 package jpabook.jpashop;
 
+import jpabook.jpashop.domain.Member;
+import jpabook.jpashop.repository.MemberRepository;
 import org.assertj.core.api.Assertions;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -14,25 +16,4 @@ import static org.junit.Assert.*;
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class MemberRepositoryTest {
-
-    @Autowired MemberRepository memberRepository;
-
-    @Test
-    @Transactional
-    @Rollback(false)
-    public void testMember() throws Exception{
-        //given
-        Member member = new Member();
-        member.setUserName("memberA");
-
-        //when
-        Long savedId = memberRepository.save(member);
-        Member findMember = memberRepository.find(savedId);
-
-        //then
-        Assertions.assertThat(findMember.getId()).isEqualTo(member.getId());
-        Assertions.assertThat(findMember.getUserName()).isEqualTo(member.getUserName());
-        Assertions.assertThat(findMember).isEqualTo(member);
-        System.out.println("findMember == member : " + (findMember == member));
-    }
 }
